af6ec0e247f45e2002f840a6b316f3da77b04b5a
Ankit-26 <sharma.ankit2698@gmail.com>
2023-09-17 01:12:15 +0530

    desktop view text

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import ToDoList from "@/components/ToDoList";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="min-h-[95vh] mt-[5vh] flex w-full">
+    <main className="max-md:min-h-screen max-md:flex max-md:justify-center max-md:items-center">
+      <div className="min-h-[95vh] mt-[5vh] flex w-full max-md:hidden">
         <SideBar />
         <section className="p-3 w-full flex flex-col">
           <Header />
@@ -25,6 +25,9 @@ export default function Home() {
             </div>
           </div>
         </section>
+      </div>
+      <div className="text-2xl p-36 text-justify">
+        This App is not suitable for mobile view. Please swicth to desktop view.
       </div>
     </main>
   );
